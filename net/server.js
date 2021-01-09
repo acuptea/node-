940ffb2eff446a7e59819f9115c6e9156fe1c71a
acuptea <418662565@qq.com>
2021-01-09 17:51:02 +0800

    newlearn

--- net/server.js
+++ net/server.js
@@ -2,8 +2,7 @@ const net = require('net')
 
 const server = net.createServer(socket=> {
   socket.on('data',function(buffer) {
-    console.log(buffer)
-    console.log(buffer, buffer.toString())
+    // const lessonId = buffer.
   })
 })
 
